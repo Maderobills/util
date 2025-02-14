5aeb81aa491d64e7174e6db5ccf4be16493af91a
Maderobills <dennisbortey60@gmail.com>
2025-02-14 00:25:03 +0000

    text comp

--- src/components/ui/Text.vue
+++ src/components/ui/Text.vue
@@ -1,4 +1,21 @@
-
+<script setup>
+</script>
 <template>
-    <p class="text">TextHere</p>
+    <div class="text-foreground dark:text-dark-foreground font-extrabold">
+    <h1 class="2xl:text-9xl xl:text-8xl md:text-4xl text-2xl">BANNER TEXT</h1>
+    <h2 class="xl:text-4xl md:text-2xl text-xl ">HEADING TEXT 1</h2>
+    <h3 class="xl:text-3xl md:text-xl text-lg ">HEADING TEXT 2</h3>
+
+</div>
+<div class="text-foreground dark:text-dark-foreground font-medium">
+    <h3 class="xl:text-3xl md:text-xl text-lg ">SUBTITLE TEXT 1</h3>
+    <h4 class=" xl:text-2xl md:text-lg text-base ">SUBTITLE TEXT 2</h4>
+
+</div>
+<div class="text-foreground dark:text-dark-foreground font-normal">
+
+    <h4 class=" 2xl:text-2xl lg:text-lg md:text-base text-xs">BODY TEXT </h4>
+
+</div>
+
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup>
-
+import Text from '../components/ui/Text.vue'
 </script>
 
 <template>
-  <h1 class="text-4xl font-bold text-center text-foreground dark:text-dark-foreground">
-    Home View
-  </h1>
+  <Text/>
 </template>
